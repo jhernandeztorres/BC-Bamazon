57a605382644fc50dd319fe6f35a3198ac1efa01
Jose Hernandez Torres <jose@hernandezt.com>
2019-01-23 21:23:41 -0700

    password removed

--- bamazonManager.js
+++ bamazonManager.js
@@ -20,7 +20,7 @@ const connection = mysql.createConnection({
     host: "localhost",
     port: 3306,
     user: "root",
-    password: "root1234",
+    password: "",
     database: "bamazonDb"
 });
 
